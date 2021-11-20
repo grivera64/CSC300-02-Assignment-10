0d4fd2f799d0a145ba444c70e0d07cfe0d1a3d00
Giovanni Rivera <grivera64@toromail.csudh.edu>
2021-11-20 00:13:04 -0800

    :memo: chore: update contribution and update check

--- WordTypeCount.java
+++ WordTypeCount.java
@@ -1,5 +1,15 @@
 //package assignment10;
 
+/*
+ *
+ * Name: AccessEntriesFromLinkedHashMapExample.java
+ * Purpose: Tests how Linked HashMaps work with replacing and
+ *          checking contents
+ * Author: grivera64
+ * Date: 11/24/2021
+ *
+ */
+
 import java.util.Map;
 import java.util.HashMap;
 import java.util.Set;
@@ -20,6 +30,12 @@ public class WordTypeCount {
         tokens = setOfWords.split(" ");
 
         for (String element : tokens) {
+
+            if (element.isBlank() || element.isEmpty()) {
+
+                continue;
+
+            }
 
             tempWord = element.toLowerCase();
 
